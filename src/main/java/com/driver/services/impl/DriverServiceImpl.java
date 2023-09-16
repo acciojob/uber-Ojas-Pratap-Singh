5d441945b549a5785d0bb3e9f272a5479da00997
ojas <singhojas3299@gmaol.com>
2023-09-16 18:01:04 +0530

    uber

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -56,6 +56,7 @@ public class DriverServiceImpl implements DriverService {
 	public void updateStatus(int driverId){
 		//Set the status of respective car to unavailable
 		Driver driver = driverRepository3.findById(driverId).get();
+
 		Cab cab = driver.getCab();
 		cab.setAvailable(false);
 		driverRepository3.save(driver);
